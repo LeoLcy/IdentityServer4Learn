cd1ddafaa00e0d110a48bd14767bda1a127ca54a
leo_lcy <406131001@qq.com>
2019-08-30 10:19:37 +0800

    增加Identity项目

---
+++ IdentityServer4Learn.MS.Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IdentityServer4Learn.MS.Identity.Data;
+using IdentityServer4Learn.MS.Identity.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityServer4Learn.MS.Identity.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityServer4Learn.MS.Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
